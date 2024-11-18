09c5e73d85ab856a042c3faa5acd80ff7484b1e7
IlungaNday <ilunganday2002@gmail.com>
2024-11-18 18:18:24 -0500

    second commit

---
+++ .upm/my_new_directory/terminal_script.sh
@@ -0,0 +1,8 @@
+#!/bin?sh
+#This is shell Script 
+You can write Terminal commands in here and they will work 
+# all execute when you 'run' this script 
+echo Start shell script 
+ls 
+pwd 
+echo End shell script 
